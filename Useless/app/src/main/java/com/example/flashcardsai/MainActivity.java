294ce9c9cd4bb6b036969bf975b29f3beb3d38a6
PedroPabloC <56356015+PedroPabloC@users.noreply.github.com>
2021-01-20 08:19:31 -0600

    Last

--- Useless/app/src/main/java/com/example/flashcardsai/MainActivity.java
+++ Useless/app/src/main/java/com/example/flashcardsai/MainActivity.java
@@ -12,7 +12,33 @@ import android.widget.Button;
 import android.widget.TextView;
 
 //API AI
+import com.example.flashcardsai.helpers.EnviarConsultas;
+import com.example.flashcardsai.interfaces.BotReply;
+import com.google.cloud.dialogflow.v2.TextInput;
 import com.google.gson.JsonElement;
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+=======
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.dialogflow.v2.DetectIntentResponse;
+import com.google.cloud.dialogflow.v2.QueryInput;
+import com.google.cloud.dialogflow.v2.SessionName;
+import com.google.cloud.dialogflow.v2.SessionsClient;
+import com.google.cloud.dialogflow.v2.SessionsSettings;
+import java.util.UUID;
+
+//Extras
+import com.google.common.collect.Lists;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 
 import java.util.Map;
 
@@ -23,7 +49,7 @@ import ai.api.model.AIError;
 import ai.api.model.AIResponse;
 import ai.api.model.Result;
 
-public class MainActivity extends AppCompatActivity implements AIListener, View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements /*AIListener, View.OnClickListener,*/ BotReply {
 
     private Button btnVoice;
     private TextView tvResult;
@@ -39,15 +65,31 @@ public class MainActivity extends AppCompatActivity implements AIListener, View.
         tvResult = (TextView)findViewById(R.id.tv_result_main);
 
         validarPermisos();
+        setUpBot();
 
+
+
+
+
+
+/*
         final AIConfiguration config = new AIConfiguration("AIzaSyAjxDzQz6i_T1aazeyLPtli8QegPB5G4YA",
                 AIConfiguration.SupportedLanguages.Spanish,
                 AIConfiguration.RecognitionEngine.System);
 
         aiService = AIService.getService(this, config);
-        aiService.setListener(this);
+        aiService.setListener(this);*/
 
-        btnVoice.setOnClickListener(this);
+        btnVoice.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                String message = "Alarma a las 8:50 a.m hasta el 20 de enero del 2021";
+                if (!message.isEmpty()) {
+                    sendMessageToBot(message);
+                } else {
+                    tvResult.setText("Errorrrr");
+                }
+            }
+    });
     }
 
     public void validarPermisos(){
@@ -61,6 +103,55 @@ public class MainActivity extends AppCompatActivity implements AIListener, View.
         }
     }
 
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+=======
+    private void setUpBot() {
+        try {
+            InputStream stream = this.getResources().openRawResource(R.raw.credentials);
+            GoogleCredentials credentials = GoogleCredentials.fromStream(stream)
+                    .createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
+            String projectId = ((ServiceAccountCredentials) credentials).getProjectId();
+
+            SessionsSettings.Builder settingsBuilder = SessionsSettings.newBuilder();
+            SessionsSettings sessionsSettings = settingsBuilder.setCredentialsProvider(
+                    FixedCredentialsProvider.create(credentials)).build();
+            sessionsClient = SessionsClient.create(sessionsSettings);
+            sessionName = SessionName.of(projectId, uuid);
+
+            Log.d(TAG, "projectId : " + projectId);
+        } catch (Exception e) {
+            Log.d(TAG, "setUpBot: " + e.getMessage());
+        }
+    }
+
+    private void sendMessageToBot(String message) {
+        QueryInput input = QueryInput.newBuilder()
+                .setText(TextInput.newBuilder().setText(message).setLanguageCode("es")).build();
+        new EnviarConsultas(this, sessionName, sessionsClient, input).execute();
+    }
+
+    @Override
+    public void callback(DetectIntentResponse returnResponse) {
+        if(returnResponse!=null) {
+            String botReply = returnResponse.getQueryResult().getFulfillmentText();
+            tvResult.setText(botReply);
+            if(!botReply.isEmpty()){
+                tvResult.setText(botReply);
+            }else {
+                tvResult.setText("Errorrr");
+            }
+        } else {
+            tvResult.setText("Null response");
+        }
+    }
+
+
+
+/*
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
     @Override
     public void onResult(AIResponse response) {
         Result result = response.getResult();
@@ -112,5 +203,6 @@ public class MainActivity extends AppCompatActivity implements AIListener, View.
                 break;
         }
 
-    }
+    }*/
+
 }
